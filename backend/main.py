c491c7264d571329852a5d374fe9f5e7606f3eca
Anya Mozhegova <asmozhegova@edu.hse.ru>
2024-02-04 22:17:36 +0400

    Added settings for fastapi and connection to mongodb

---
+++ backend/main.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+import logging
+import os
+
+from fastapi import FastAPI
+from starlette.requests import Request
+from starlette.responses import Response
+from mongoengine import connect
+
+logging.basicConfig(level=logging.INFO,
+                    format="%(levelname)s:  %(asctime)s  %(message)s",
+                    datefmt="%Y-%m-%d %H:%M:%S")
+
+app = FastAPI(docs_url=None, redoc_url=None)
+
+MONGODB_URL = os.getenv("MONGODB_URL", "mongodb://admin:123@mongo:27017/analyticsDB")
+connect(host=MONGODB_URL)
+
+
+@app.middleware("http")
+async def add_cors_headers(request: Request, call_next):
+    response = await call_next(request)
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "*"
+    response.headers["Access-Control-Request-Method"] = "*"
+    response.headers["Access-Control-Allow-Headers"] = "*"
+    response.headers["Access-Control-Request-Headers"] = "content-type"
+    response.headers["Access-Control-Allow-Private-Network"] = "true"
+    response.headers["Access-Control-Max-Age"] = "86400"
+    return response
+
+
+@app.get("/")
+async def root():
+    logging.info("Root application started successfully")
+
+
+@app.options("/{full_path:path}")
+def options_handler(r: Request, full_path: str | None):
+    headers = {"Access-Control-Allow-Origin": "*",
+               "Access-Control-Allow-Methods": "*",
+               "Access-Control-Allow-Headers": "Content-Type"}
+    return Response(status_code=200, headers=headers)
