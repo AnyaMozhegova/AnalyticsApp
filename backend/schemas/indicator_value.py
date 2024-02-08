2394559de6cb08a19365ba8e3b8efb9c9b1da480
AnyaMozhegova <asmozhegova@edu.hse.ru>
2024-02-09 02:14:55 +0400

    Added get pydantic models

--- backend/schemas/indicator_value.py
+++ backend/schemas/indicator_value.py
@@ -4,3 +4,21 @@ from pydantic import BaseModel
 class IndicatorValueCreate(BaseModel):
     report_indicator: int
     column: int
+
+
+class IndicatorValueGetBase(BaseModel):
+    user: int
+    report: int
+    column: int
+
+
+class IndicatorValuesGet(IndicatorValueGetBase):
+    pass
+
+
+class IndicatorValueGet(IndicatorValueGetBase):
+    indicator_value: int
+
+
+class IndicatorValueGetByName(IndicatorValueGetBase):
+    report_indicator_name: str
