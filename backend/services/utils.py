f34fb326ba85df1250b720e1a53ef0592c0f8dfb
AnyaMozhegova <asmozhegova@edu.hse.ru>
2024-02-08 05:10:17 +0400

    Added method to filter none values in column

--- backend/services/utils.py
+++ backend/services/utils.py
@@ -1,10 +1,12 @@
-from typing import Optional
+from typing import List, Optional
 
 from models.role import Role
 from models.user import User
 
 from errors.forbidden import ForbiddenError
 from errors.not_found import NotFoundError
+
+from models.report_column import ReportColumn
 
 
 def models_to_dict(objs: list):
@@ -25,3 +27,8 @@ def check_customer_access(user_id: int) -> Optional[bool]:
             return True
         raise NotFoundError("User is not found")
     raise ForbiddenError("User doesn't have customer access")
+
+
+def filter_column_data(column: ReportColumn) -> Optional[List[float]]:
+    filtered_data = [value for value in column.column_data if value is not None]
+    return filtered_data if len(filtered_data) > 0 else None
