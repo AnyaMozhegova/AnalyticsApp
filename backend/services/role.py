01e2711ee0cd786eebd061f0fef1197e0cf31cf4
AnyaMozhegova <asmozhegova@edu.hse.ru>
2024-02-10 20:51:03 +0400

    Removed to dict convert in role service

--- backend/services/role.py
+++ backend/services/role.py
@@ -1,32 +1,34 @@
+from typing import List, Optional
+
 from errors.not_found import NotFoundError
 from models.role import Role
 from schemas.role import RoleCreate, RoleUpdate
-from services.utils import check_admin_access, models_to_dict
+from services.utils import check_admin_access
 
 
-def get_roles(user_id: int):
+def get_roles(user_id: int) -> List[Role]:
     check_admin_access(user_id)
     roles = Role.objects()
-    return models_to_dict(roles)
+    return roles
 
 
-def get_role(user_id: int, role_id: int):
+def get_role(user_id: int, role_id: int) -> Optional[Role]:
     check_admin_access(user_id)
     if role := Role.objects(id=role_id).first():
-        return role.to_dict()
+        return role
     raise NotFoundError(
         f"Could not get role with id = {role_id}. There is no such entity")
 
 
-def get_role_by_name(user_id: int, role_name: str):
+def get_role_by_name(user_id: int, role_name: str) -> Optional[Role]:
     check_admin_access(user_id)
     if role := Role.objects(name=role_name).first():
-        return role.to_dict()
+        return role
     raise NotFoundError(
         f"Could not get role with name = {role_name}. There is no such entity")
 
 
-def update_role(user_id: int, role_id: int, role_update: RoleUpdate):
+def update_role(user_id: int, role_id: int, role_update: RoleUpdate) -> Optional[Role]:
     check_admin_access(user_id)
     if role := Role.objects(id=role_id).first():
         for field, value in role_update.dict(exclude_unset=True).items():
