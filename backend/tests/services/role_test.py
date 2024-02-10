47f34505f6e51085da3c852f8ef9085caba85d37
AnyaMozhegova <asmozhegova@edu.hse.ru>
2024-02-10 22:59:19 +0400

    Added fixture in tests

--- backend/tests/services/role_test.py
+++ backend/tests/services/role_test.py
@@ -7,69 +7,66 @@ from models.role import Role
 from schemas.role import RoleUpdate
 from services.role import get_role, get_role_by_name, get_roles, update_role
 
+from tests.conftest import clean_up_test, connect_test, create_admin_user
+
 ROLE_1_NAME = "Role_1"
 ROLE_2_NAME = "Role_2"
 
 
-def create_role_utils(role_name):
-    role = Role(name=role_name).save()
-    return role.id
+@pytest.fixture(scope="function")
+def db_setup():
+    db_name = "test_db"
+    connect_test(db_name)
+    user = create_admin_user()
+    role = Role(name=ROLE_1_NAME).save()
+    yield role, user
+    clean_up_test(db_name)
 
 
-def test_create_role(admin_user):
-    role_id = create_role_utils(ROLE_1_NAME)
-    assert role_id is not None
-    assert Role.objects(name=ROLE_1_NAME).first() is not None
-
-
-def test_create_role_duplicate_name(admin_user):
-    create_role_utils(ROLE_1_NAME)
-    with pytest.raises(NotUniqueError):
-        create_role_utils(ROLE_1_NAME)
-
-
-def test_get_roles(admin_user):
-    create_role_utils(ROLE_1_NAME)
-    create_role_utils(ROLE_2_NAME)
+def test_get_roles(db_setup):
+    _, admin_user = db_setup
     roles = get_roles(user_id=admin_user.id)
-    assert len(roles) == 3  # With admin role
+    assert len(roles) == 2  # With admin role
 
 
-def test_get_role(admin_user):
-    role_id = create_role_utils(ROLE_1_NAME)
-    result = get_role(user_id=admin_user.id, role_id=role_id)
+def test_get_role(db_setup):
+    role, admin_user = db_setup
+    result = get_role(user_id=admin_user.id, role_id=role.id)
     assert result['name'] == ROLE_1_NAME
 
 
-def test_get_role_by_name(admin_user):
-    role_id = create_role_utils(ROLE_1_NAME)
+def test_get_role_by_name(db_setup):
+    role, admin_user = db_setup
     result = get_role_by_name(user_id=admin_user.id, role_name=ROLE_1_NAME)
-    assert result['id'] == role_id
+    assert result['id'] == role.id
 
 
-def test_get_role_non_existent_id(admin_user):
+def test_get_role_non_existent_id(db_setup):
     non_existent_id = 999
+    _, admin_user = db_setup
 
     with pytest.raises(NotFoundError):
         get_role(user_id=admin_user.id, role_id=non_existent_id)
 
 
-def test_get_role_by_name_non_existent(admin_user):
+def test_get_role_by_name_non_existent(db_setup):
     non_existent_name = "NonExistentName"
+    _, admin_user = db_setup
 
     with pytest.raises(NotFoundError):
         get_role_by_name(user_id=admin_user.id, role_name=non_existent_name)
 
 
-def test_update_role(admin_user):
-    role_id = create_role_utils(ROLE_1_NAME)
+def test_update_role(db_setup):
+    role, admin_user = db_setup
     update_data = RoleUpdate(name=ROLE_2_NAME)
-    updated_role = update_role(user_id=admin_user.id, role_id=role_id,
+    updated_role = update_role(user_id=admin_user.id, role_id=role.id,
                                role_update=update_data)
     assert updated_role.name == ROLE_2_NAME
 
 
-def test_update_role_non_existent_id(admin_user):
+def test_update_role_non_existent_id(db_setup):
+    _, admin_user = db_setup
     non_existent_id = 999
     update_data = RoleUpdate(name=ROLE_2_NAME)
 
@@ -78,17 +75,18 @@ def test_update_role_non_existent_id(admin_user):
                     role_update=update_data)
 
 
-def test_update_role_duplicated_name(admin_user):
-    create_role_utils(ROLE_1_NAME)
-    role_id = create_role_utils(ROLE_2_NAME)
+def test_update_role_duplicated_name(db_setup):
+    _, admin_user = db_setup
+    role = Role(name=ROLE_2_NAME).save()
     update_data = RoleUpdate(name=ROLE_1_NAME)
 
     with pytest.raises(NotUniqueError):
-        update_role(user_id=admin_user.id, role_id=role_id,
+        update_role(user_id=admin_user.id, role_id=role.id,
                     role_update=update_data)
 
 
-def test_unauthorized_access(mocker, admin_user):
+def test_unauthorized_access(mocker, db_setup):
     mocker.patch('services.utils.check_admin_access', side_effect=ForbiddenError("User doesn't have admin access"))
+    _, admin_user = db_setup
     with pytest.raises(ForbiddenError):
         get_roles(user_id=admin_user.id + 1)
