980e9b58da1cf1f8dc22c3b55eb911b6f41e5171
AnyaMozhegova <asmozhegova@edu.hse.ru>
2024-02-24 23:13:15 +0400

    Made admin model

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,17 +1,17 @@
-from mongoengine import DO_NOTHING, Document, ReferenceField, SequenceField, StringField, BooleanField
-
-from models.role import Role
-
 from errors.not_found import NotFoundError
+from models.role import Role
+from mongoengine import BooleanField, DO_NOTHING, Document, ReferenceField, SequenceField, StringField
 
 
 class User(Document):
     id = SequenceField(primary_key=True)
     name = StringField(required=True)
-    email = StringField(required=True, unique=True)
+    email = StringField(required=True)
     password = StringField(required=True)
     is_active = BooleanField(default=True)
     role = ReferenceField(Role, reverse_delete_rule=DO_NOTHING)
+
+    meta = {'allow_inheritance': True}
 
     def to_dict(self):
         if self.is_active:
