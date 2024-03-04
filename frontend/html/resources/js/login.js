f4e135b4dc8e0615dce542dad3f54b78024bb0ea
Anna <144148299+AnyaMozhegova@users.noreply.github.com>
2024-03-04 19:46:14 +0400

    33 login page (#42)
    * Added role name in return content
    
    * Removed link to logo
    
    * Added login page

--- frontend/html/resources/js/login.js
+++ frontend/html/resources/js/login.js
@@ -1,12 +1,36 @@
 document.body.addEventListener('htmx:afterOnLoad', function (event) {
-    // Check if the response contains JSON content
+    const status = event.detail.xhr.status;
     const contentType = event.detail.xhr.getResponseHeader("Content-Type");
-    if (contentType && contentType.indexOf("application/json") !== -1) {
-        // Parse the JSON response
+
+    if (contentType && contentType.indexOf("application/json") !== -1 && status === 200) {
         const response = JSON.parse(event.detail.xhr.responseText);
         if (response.id) {
-            // Assuming the backend response contains an "id" field
             sessionStorage.setItem('userId', response.id);
+            Swal.fire({
+                title: 'Success!',
+                text: 'Login is successful!',
+                icon: 'success',
+                confirmButtonText: 'OK'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    if (response.role === "customer")  window.location.href = 'http://0.0.0.0:3001/home';
+                    else if (response.role === "admin") window.location.href = 'http://0.0.0.0:3001/admin_home';
+                }
+            });
         }
+    } else if (status === 400) {
+        Swal.fire({
+            title: 'Error!',
+            text: 'User data is incorrect. Make sure the fields are not empty and your account exists',
+            icon: 'error',
+            confirmButtonText: 'OK'
+        });
+    } else if (status === 500) {
+        Swal.fire({
+            title: 'Server Error!',
+            text: 'An error occurred on the server. Please try again later.',
+            icon: 'error',
+            confirmButtonText: 'OK'
+        });
     }
 });
