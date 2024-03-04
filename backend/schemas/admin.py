a42af21ce59a3a702d1942ba39ed6b61e8637aaa
AnyaMozhegova <asmozhegova@edu.hse.ru>
2024-03-04 15:36:38 +0400

    Added current user dependency

--- backend/schemas/admin.py
+++ backend/schemas/admin.py
@@ -1,13 +1,8 @@
-from pydantic import BaseModel
 from schemas.user import UserCreate, UserUpdate
 
 
 class AdminCreate(UserCreate):
     parent_admin: int
-
-
-class AdminDelete(BaseModel):
-    admin_to_delete: int
 
 
 class AdminUpdate(UserUpdate):
