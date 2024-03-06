264bc3c0ea8217184d9e9c3ad1f8243a8cbc4d0b
AnyaMozhegova <asmozhegova@edu.hse.ru>
2024-03-06 20:33:34 +0400

    Fixed deleting migration

--- backend/migrations/2024_02_23_001_make_main_user_admin.py
+++ backend/migrations/2024_02_23_001_make_main_user_admin.py
@@ -1,13 +1,10 @@
 import logging
 import os
 
-from mongoengine import connect
-
-from models.role import Role
-from models.user import User
-from passlib.handlers.bcrypt import bcrypt
-
 from models.admin import Admin
+from models.role import Role
+from mongoengine import connect
+from passlib.handlers.bcrypt import bcrypt
 
 MONGODB_URL = os.getenv("MONGODB_URL")
 MAIN_USER_EMAIL = os.getenv("MAIN_USER_EMAIL")
@@ -25,9 +22,6 @@ class Migration:
 
         admin_role = Role.objects(name='Admin').first()
 
-        if main_user := User.objects(email=MAIN_USER_EMAIL, is_active=True).first():
-            main_user.delete()
-            logging.info(f"Main user with id = {main_user.id} deleted.")
         if not Admin.objects(email=MAIN_USER_EMAIL, is_active=True).first():
             admin: Admin = Admin(name=MAIN_USER_NAME, email=MAIN_USER_EMAIL, password=bcrypt.hash(MAIN_USER_PASSWORD),
                                  role=admin_role).save()
