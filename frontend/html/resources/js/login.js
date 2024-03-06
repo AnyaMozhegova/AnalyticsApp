bacce539f0c4a738d1895dcf2fe33086bb40d07c
AnyaMozhegova <asmozhegova@edu.hse.ru>
2024-03-06 20:56:41 +0400

    Replaced htmx with vanilla js

--- frontend/html/resources/js/login.js
+++ frontend/html/resources/js/login.js
@@ -1,10 +1,39 @@
-document.body.addEventListener('htmx:afterOnLoad', function (event) {
-    const status = event.detail.xhr.status;
-    const contentType = event.detail.xhr.getResponseHeader("Content-Type");
+async function login() {
+    const email = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    const loginUrl = "http://localhost:8001/login";
 
-    if (contentType && contentType.indexOf("application/json") !== -1 && status === 200) {
-        const response = JSON.parse(event.detail.xhr.responseText);
-        if (response.id) {
+    // Make the first POST request
+    await fetch(loginUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: new URLSearchParams({
+            username: email,
+            password: password
+        },),
+        credentials: "include"
+    })
+        .then(async response => {
+                if (response.status === 401 || response.status === 400) {
+                    Swal.fire({
+                        title: 'Error!',
+                        text: 'Incorrect email or password. Verify your input data',
+                        icon: 'error',
+                        confirmButtonText: 'OK'
+                    })
+                } else if (response.status === 500) {
+                    Swal.fire({
+                        title: 'Server Error!',
+                        text: 'An error occurred on the server. Please try again later.',
+                        icon: 'error',
+                        confirmButtonText: 'OK'
+                    });
+                } else return await response.json()
+            }
+        )
+        .then(async response => {
             sessionStorage.setItem('userId', response.id);
             Swal.fire({
                 title: 'Success!',
@@ -13,24 +42,10 @@ document.body.addEventListener('htmx:afterOnLoad', function (event) {
                 confirmButtonText: 'OK'
             }).then((result) => {
                 if (result.isConfirmed) {
-                    if (response.role === "customer")  window.location.href = 'http://localhost:3001/home';
+                    if (response.role === "customer") window.location.href = 'http://localhost:3001/home';
                     else if (response.role === "admin") window.location.href = 'http://localhost:3001/admin_home';
                 }
             });
-        }
-    } else if (status === 400) {
-        Swal.fire({
-            title: 'Error!',
-            text: 'User data is incorrect. Make sure the fields are not empty and your account exists',
-            icon: 'error',
-            confirmButtonText: 'OK'
-        });
-    } else if (status === 500) {
-        Swal.fire({
-            title: 'Server Error!',
-            text: 'An error occurred on the server. Please try again later.',
-            icon: 'error',
-            confirmButtonText: 'OK'
-        });
-    }
-});
+        })
+}
+
