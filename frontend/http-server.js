7e90b3d25f120dee0737b0032ff38c3c311ef33a
AnyaMozhegova <asmozhegova@edu.hse.ru>
2024-03-07 18:01:01 +0400

    Added regex for report pages

--- frontend/http-server.js
+++ frontend/http-server.js
@@ -4,12 +4,15 @@ const fs = require('fs');
 const hostname = '0.0.0.0';
 const port = 8000;
 
-
 const server = http.createServer((req, response) => {
     console.log(`Serving request for ${req.url}`);
     let filePath;
 
-    if (req.url.endsWith('.css') || req.url.endsWith('.js') || req.url.endsWith('.ico')) {
+    const reportRegEx = /^\/report\/(\d+)$/;
+
+    if (reportRegEx.test(req.url)) {
+        filePath = './html/report.html';
+    } else if (req.url.endsWith('.css') || req.url.endsWith('.js') || req.url.endsWith('.ico')) {
         filePath = `.${req.url}`;
     } else {
         filePath = `./html${req.url === '/' ? '/index.html' : req.url}`;
@@ -17,19 +20,16 @@ const server = http.createServer((req, response) => {
             filePath += '.html';
         }
     }
+
     fs.readFile(filePath, (error, data) => {
         if (error) {
             console.error(`Error reading file: ${error}`);
             response.writeHead(404, {'Content-Type': 'text/plain'});
             response.end('File not found');
         } else {
-            if (req.url.endsWith(".css")) {
-                response.writeHead(200, {'Content-Type': 'text/css'});
-                response.end(data);
-            } else {
-                response.writeHead(200, {'Content-Type': 'text/html'});
-                response.end(data);
-            }
+            const contentType = filePath.endsWith(".css") ? 'text/css' : 'text/html';
+            response.writeHead(200, {'Content-Type': contentType});
+            response.end(data);
         }
     });
 });
